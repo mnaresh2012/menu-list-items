a4d577794dbcb99962d4ac4173e4b139a0db6d53
Naresh Mahanta <nmahanta2@sapient.com>
2020-10-13 05:26:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className = 'container'>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+export default App;
